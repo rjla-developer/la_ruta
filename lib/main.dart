a80ef19d2e91df36eb8063189a77321a4f8376f7
RJ Luengas <robertojla8@gmail.com>
2024-02-10 14:33:52 -0600

    Se instalo: 	- flutter_map: ^6.1.0 	- latlong2: ^0.9.0
    Y se hicieron las modificaciones solicitadas por el paquete del mapa.
    Se creo home con un mapa por defecto que se ya funciona en el emulador

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
+//Screens:
+import 'screens/home.dart';
+
 void main() {
-  runApp(const Home());
+  runApp(const MyApp());
 }
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      title: 'La Ruta',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const Home(),
+    );
   }
 }
---
+++ lib/screens/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+
+//Latlong2:
+import 'package:latlong2/latlong.dart';
+
+const mapboxAccessToken =
+    "sk.eyJ1IjoicmotZGV2ZWxvcGVyIiwiYSI6ImNsc2dkazgzdTFsbjIybG8wMmFtcXVwODMifQ.gJl_3nLWEv_E9SeT6H_PkQ";
+
+const myPosition = LatLng(0, 0);
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: const MapOptions(
+        initialCenter: myPosition,
+        initialZoom: 3.0,
+      ),
+      children: [
+        TileLayer(
+          urlTemplate:
+              "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=$mapboxAccessToken",
+          additionalOptions: const {
+            'accessToken': mapboxAccessToken,
+            'id': 'mapbox/streets-v11',
+          },
+        ),
+      ],
+    );
+  }
+}
