818b204e9e78d92c03d3156384c3f39ad319bfd9
RJDalefon <rjluengas@dalefon.mx>
2024-02-15 10:18:19 -0600

    Se hizo un ejemplo para obtener el trazo de un trayecto en el mapa con base a la API de navegación de Mapbox

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,14 +3,46 @@ import 'package:flutter_map/flutter_map.dart';
 
 //Latlong2:
 import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 const mapboxAccessToken =
     "sk.eyJ1IjoicmotZGV2ZWxvcGVyIiwiYSI6ImNsc2dkazgzdTFsbjIybG8wMmFtcXVwODMifQ.gJl_3nLWEv_E9SeT6H_PkQ";
 
 const myPosition = LatLng(18.9213, -99.2347);
+const destinationPosition = LatLng(18.92330, -99.23580);
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<LatLng> routePoints = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchRoute();
+  }
+
+  Future<void> fetchRoute() async {
+    final response = await http.get(Uri.parse(
+        'https://api.mapbox.com/directions/v5/mapbox/walking/${myPosition.longitude},${myPosition.latitude};${destinationPosition.longitude},${destinationPosition.latitude}?geometries=geojson&access_token=$mapboxAccessToken'));
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final geometry = data['routes'][0]['geometry']['coordinates'];
+      final List<LatLng> points =
+          geometry.map<LatLng>((coord) => LatLng(coord[1], coord[0])).toList();
+      setState(() {
+        routePoints = points;
+      });
+    } else {
+      throw Exception('Failed to load route');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +51,7 @@ class Home extends StatelessWidget {
         FlutterMap(
           options: const MapOptions(
             initialCenter: myPosition,
-            initialZoom: 18.5,
+            initialZoom: 17.5,
             maxZoom: 22,
           ),
           children: [
@@ -29,6 +61,15 @@ class Home extends StatelessWidget {
               additionalOptions: const {
                 'accessToken': mapboxAccessToken,
               },
+            ),
+            PolylineLayer(
+              polylines: [
+                Polyline(
+                  strokeWidth: 4.0,
+                  points: routePoints,
+                  color: Colors.blue,
+                ),
+              ],
             ),
             const MarkerLayer(
               markers: [
@@ -48,17 +89,13 @@ class Home extends StatelessWidget {
                     color: Color.fromARGB(255, 26, 180, 54),
                   ),
                 ),
-              ],
-            ),
-            PolylineLayer(
-              polylines: [
-                Polyline(
-                  points: [
-                    myPosition,
-                    const LatLng(18.9216, -99.23367),
-                    const LatLng(18.9225, -99.23479),
-                  ],
-                  color: Colors.blue,
+                Marker(
+                  point: LatLng(18.92330, -99.23580),
+                  child: Icon(
+                    Icons.directions_bus,
+                    size: 50.0,
+                    color: Color.fromARGB(255, 189, 39, 194),
+                  ),
                 ),
               ],
             ),
