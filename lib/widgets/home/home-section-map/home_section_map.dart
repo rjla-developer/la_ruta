5d0c41b629a5635ee29b140e3adb1d0a64fb0912
RJ Luengas <robertojla8@gmail.com>
2024-02-25 17:46:07 -0600

    Se hizo la lógica para determinar la posición del usuario y pedirle permiso para obtenerla

--- lib/widgets/home/home-section-map/home_section_map.dart
+++ lib/widgets/home/home-section-map/home_section_map.dart
@@ -6,6 +6,9 @@ import 'package:flutter_map/flutter_map.dart';
 //Latlong2:
 import 'package:latlong2/latlong.dart';
 
+//Geolocator:
+import 'package:geolocator/geolocator.dart';
+
 const mapboxAccessToken =
     "sk.eyJ1IjoicmotZGV2ZWxvcGVyIiwiYSI6ImNsc2dkazgzdTFsbjIybG8wMmFtcXVwODMifQ.gJl_3nLWEv_E9SeT6H_PkQ";
 const myPosition = LatLng(18.788646, -99.242968);
@@ -19,6 +22,40 @@ class HomeSectionMap extends StatefulWidget {
 }
 
 class _HomeSectionMapState extends State<HomeSectionMap> {
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Por favor, habilita los servicios de ubicación.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Los permisos de ubicación son denegados');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Los permisos de ubicación están permanentemente denegados, no podemos solicitar permisos.');
+    }
+
+    var myPosition = await Geolocator.getCurrentPosition();
+    print(myPosition);
+    setState(() {});
+    return myPosition;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
@@ -46,14 +83,14 @@ class _HomeSectionMapState extends State<HomeSectionMap> {
         ),
         const MarkerLayer(
           markers: [
-            /* Marker(
-                  point: myPosition,
-                  child: Icon(
-                    Icons.person_pin,
-                    size: 50.0,
-                    color: Color.fromARGB(255, 25, 176, 218),
-                  ),
-                ), */
+            Marker(
+              point: myPosition,
+              child: Icon(
+                Icons.person_pin,
+                size: 50.0,
+                color: Color.fromARGB(255, 25, 176, 218),
+              ),
+            ),
             Marker(
               point: LatLng(18.940714, -99.241622),
               child: Icon(
