95655073f937c6904c9571c8eff09a5118c58c02
RJ Luengas <robertojla8@gmail.com>
2024-02-25 22:24:27 -0600

    Se acomodo y se coloco un Market justo en la posición del usuario que previamente se solicito. Se probo para Android e IOS su funcionamiento.

--- lib/widgets/home/home-section-map/home_section_map.dart
+++ lib/widgets/home/home-section-map/home_section_map.dart
@@ -11,7 +11,6 @@ import 'package:geolocator/geolocator.dart';
 
 const mapboxAccessToken =
     "sk.eyJ1IjoicmotZGV2ZWxvcGVyIiwiYSI6ImNsc2dkazgzdTFsbjIybG8wMmFtcXVwODMifQ.gJl_3nLWEv_E9SeT6H_PkQ";
-const myPosition = LatLng(18.788646, -99.242968);
 
 class HomeSectionMap extends StatefulWidget {
   final List<LatLng> routePoints;
@@ -22,6 +21,8 @@ class HomeSectionMap extends StatefulWidget {
 }
 
 class _HomeSectionMapState extends State<HomeSectionMap> {
+  LatLng? userPosition;
+
   Future<Position> _determinePosition() async {
     bool serviceEnabled;
     LocationPermission permission;
@@ -44,10 +45,13 @@ class _HomeSectionMapState extends State<HomeSectionMap> {
           'Los permisos de ubicación están permanentemente denegados, no podemos solicitar permisos.');
     }
 
-    var myPosition = await Geolocator.getCurrentPosition();
-    print(myPosition);
-    setState(() {});
-    return myPosition;
+    var userPositionDetermined = await Geolocator.getCurrentPosition();
+    /* print(userPositionDetermined); */
+    setState(() {
+      userPosition = LatLng(
+          userPositionDetermined.latitude, userPositionDetermined.longitude);
+    });
+    return userPositionDetermined;
   }
 
   @override
@@ -58,50 +62,52 @@ class _HomeSectionMapState extends State<HomeSectionMap> {
 
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: const MapOptions(
-        initialCenter: myPosition,
-        initialZoom: 10.5,
-        maxZoom: 22,
-      ),
-      children: [
-        TileLayer(
-          urlTemplate:
-              "https://api.mapbox.com/styles/v1/rj-developer/clsgye3i303gv01o88gzf40sf/tiles/256/{z}/{x}/{y}@2x?access_token=$mapboxAccessToken",
-          additionalOptions: const {
-            'accessToken': mapboxAccessToken,
-          },
-        ),
-        PolylineLayer(
-          polylines: [
-            Polyline(
-              strokeWidth: 4.0,
-              points: widget.routePoints,
-              color: Colors.green,
+    return userPosition != null
+        ? FlutterMap(
+            options: MapOptions(
+              initialCenter: userPosition!,
+              initialZoom: 17,
+              maxZoom: 22,
             ),
-          ],
-        ),
-        const MarkerLayer(
-          markers: [
-            Marker(
-              point: myPosition,
-              child: Icon(
-                Icons.person_pin,
-                size: 50.0,
-                color: Color.fromARGB(255, 25, 176, 218),
+            children: [
+              TileLayer(
+                urlTemplate:
+                    "https://api.mapbox.com/styles/v1/rj-developer/clsgye3i303gv01o88gzf40sf/tiles/256/{z}/{x}/{y}@2x?access_token=$mapboxAccessToken",
+                additionalOptions: const {
+                  'accessToken': mapboxAccessToken,
+                },
               ),
-            ),
-            Marker(
-              point: LatLng(18.940714, -99.241622),
-              child: Icon(
-                Icons.directions_bus,
-                size: 50.0,
-                color: Color.fromARGB(255, 189, 39, 194),
+              PolylineLayer(
+                polylines: [
+                  Polyline(
+                    strokeWidth: 4.0,
+                    points: widget.routePoints,
+                    color: Colors.green,
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
-      ],
-    );
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    point: userPosition!,
+                    child: const Icon(
+                      Icons.person_pin,
+                      size: 50.0,
+                      color: Color.fromARGB(255, 25, 176, 218),
+                    ),
+                  ),
+                  const Marker(
+                    point: LatLng(18.940714, -99.241622),
+                    child: Icon(
+                      Icons.directions_bus,
+                      size: 50.0,
+                      color: Color.fromARGB(255, 189, 39, 194),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )
+        : const Center(child: CircularProgressIndicator());
   }
 }
