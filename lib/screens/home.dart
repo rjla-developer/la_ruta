5918f607a66fb20f1bc925f7692b94fe51ffd5e9
RJDalefon <rjluengas@dalefon.mx>
2024-02-28 15:27:48 -0600

    Se instalo el paquete "sliding_up_panel" el cual ayuda a colocar un panel inferior para que aparezca una vez que el usuario le haya dado clic a la ubicación seleccionada a la cual quiere dirigirse.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,6 +16,9 @@ import 'package:flutter_map_animations/flutter_map_animations.dart';
 //FlutterMap:
 import 'package:flutter_map/flutter_map.dart';
 
+//SlidingUpPanel:
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
 const mapboxAccessToken =
     "sk.eyJ1IjoicmotZGV2ZWxvcGVyIiwiYSI6ImNsc2dkazgzdTFsbjIybG8wMmFtcXVwODMifQ.gJl_3nLWEv_E9SeT6H_PkQ";
 
@@ -30,13 +33,18 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
   LatLng? targetPosition;
   late final animatedMapController = AnimatedMapController(vsync: this);
   bool useTransformer = true;
+  bool show = false;
   static const useTransformerId = 'useTransformerId';
   final markers = ValueNotifier<List<AnimatedMarker>>([]);
+
+  //SlidingUpPanel:
+  final PanelController _panelController = PanelController();
 
   void setTargetPosition(LatLng position) {
     setState(() {
       targetPosition = position;
     });
+    _panelController.open();
   }
 
   @override
@@ -48,81 +56,133 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        HomeSectionMap(
-            targetPosition: targetPosition,
-            animatedMapController: animatedMapController),
-        HomeSectionSearch(setTargetPosition: setTargetPosition),
-        /* Positioned(
-          bottom: 60,
-          left: 50,
-          right: 50,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: () async {
-                  var route =
-                      await getRoute("Santa María - Buena vista - Calera");
-                  setState(() {
-                    routePoints = route;
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 15.0,
-                    horizontal: 15.0,
-                  ),
-                ),
-                child: const Text(
-                  'Santa María - Buena vista - Calera',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
+    return SlidingUpPanel(
+      renderPanelSheet: false,
+      controller: _panelController,
+      minHeight: 0,
+      maxHeight: 300.0,
+      panel: Container(
+        decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(24.0),
+                topRight: Radius.circular(24.0)),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 10.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+            ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15.0,
+                  horizontal: 15.0,
                 ),
               ),
-              const SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () async {
-                  var route = await getRoute(
-                      "Santa María - Buena vista - Francisco Villa");
-                  setState(() {
-                    routePoints = route;
-                  });
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 15.0,
-                    horizontal: 15.0,
-                  ),
+              child: const Text(
+                'Santa María - Buena vista - Calera',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
                 ),
-                child: const Text(
-                  'Santa María - Buena vista - Francisco Villa',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
+                textAlign: TextAlign.center,
               ),
-            ],
-          ),
-        ), */
-        Positioned(
-          bottom: 20,
-          right: 20,
-          child: FloatingActionButton(
-            onPressed: () => animatedMapController.animatedZoomOut(
-              customId: useTransformer ? useTransformerId : null,
             ),
-            tooltip: 'Zoom out',
-            child: const Icon(Icons.zoom_out),
-          ),
+          ],
         ),
-      ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          HomeSectionMap(
+              targetPosition: targetPosition,
+              animatedMapController: animatedMapController),
+          HomeSectionSearch(setTargetPosition: setTargetPosition),
+
+          /* Positioned(
+            bottom: 60,
+            left: 50,
+            right: 50,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                ElevatedButton(
+                  onPressed: () async {
+                    var route =
+                        await getRoute("Santa María - Buena vista - Calera");
+                    setState(() {
+                      routePoints = route;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 15.0,
+                      horizontal: 15.0,
+                    ),
+                  ),
+                  child: const Text(
+                    'Santa María - Buena vista - Calera',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () async {
+                    var route = await getRoute(
+                        "Santa María - Buena vista - Francisco Villa");
+                    setState(() {
+                      routePoints = route;
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 15.0,
+                      horizontal: 15.0,
+                    ),
+                  ),
+                  child: const Text(
+                    'Santa María - Buena vista - Francisco Villa',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ), */
+          Positioned(
+            bottom: 100,
+            right: 20,
+            child: FloatingActionButton(
+              onPressed: () => setState(() {
+                show = !show;
+              }),
+              child: const Icon(Icons.grain),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: FloatingActionButton(
+              onPressed: () => animatedMapController.animatedZoomOut(
+                customId: useTransformer ? useTransformerId : null,
+              ),
+              tooltip: 'Zoom out',
+              child: const Icon(Icons.zoom_out),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
