a624251b733678f4ce36f9bb5b43045239d49b41
RJDalefon <rjluengas@dalefon.mx>
2024-02-29 17:46:16 -0600

    Se hizo un avance en el maquetado del panel para mostrar las rutas

--- lib/widgets/home/home_section_panel.dart
+++ lib/widgets/home/home_section_panel.dart
@@ -6,38 +6,107 @@ class HomeSectionPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 255, 255, 255),
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(24.0),
-                topRight: Radius.circular(24.0)),
-            boxShadow: [
-              BoxShadow(
-                blurRadius: 10.0,
-                color: Color.fromARGB(255, 0, 0, 0),
-              ),
-            ]),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 10,
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 255, 255, 255),
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(24.0), topRight: Radius.circular(24.0)),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 10.0,
+            color: Color.fromARGB(255, 0, 0, 0),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            width: 30,
+            height: 5,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: const BorderRadius.all(Radius.circular(12.0)),
             ),
-            Container(
-              width: 30,
-              height: 5,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 10, right: 10),
+            child: Container(
               decoration: BoxDecoration(
                 color: Colors.grey[300],
                 borderRadius: const BorderRadius.all(Radius.circular(12.0)),
               ),
+              child: const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Image(
+                      image: NetworkImage(
+                        'https://queruta.mx/wp-content/uploads/2023/10/5.png',
+                      ),
+                      width: 100,
+                      height: 80,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: Column(
+                        /* crossAxisAlignment: CrossAxisAlignment.end, */
+                        children: [
+                          Text(
+                            'Dirección:',
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                          Text(
+                            'Santa María - Buena vista - Calera',
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          /* Text(
+                            'Esta ruta llega cada:',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 2,
+                          ),
+                          Text(
+                            '8 a 10 minutos',
+                            style: TextStyle(
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ), */
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Text(
-              "Panel",
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ));
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
   }
 }
