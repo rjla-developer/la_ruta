5bfb872948c9656ec31df204ea7a92308d634a83
RJ Luengas <robertojla8@gmail.com>
2024-02-29 23:16:47 -0600

    Se hizo que funcionara el que el mapa se centre entre la posición del usuario y la posición final con base a esas dos posiciones que son estados y que van cambiando

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -20,6 +20,9 @@ import 'package:flutter_map/flutter_map.dart';
 //SlidingUpPanel:
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
+//Geolocator:
+import 'package:geolocator/geolocator.dart';
+
 const mapboxAccessToken =
     "sk.eyJ1IjoicmotZGV2ZWxvcGVyIiwiYSI6ImNsc2dkazgzdTFsbjIybG8wMmFtcXVwODMifQ.gJl_3nLWEv_E9SeT6H_PkQ";
 
@@ -31,13 +34,11 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> with TickerProviderStateMixin {
-  /* LatLng? userPosition; */
+  LatLng? userPosition;
   LatLng? targetPosition;
   late final animatedMapController = AnimatedMapController(vsync: this);
-  bool useTransformer = true;
-  bool show = false;
-  static const useTransformerId = 'useTransformerId';
-  final markers = ValueNotifier<List<AnimatedMarker>>([]);
+  final bool _useTransformer = true;
+  static const _useTransformerId = '_useTransformerId';
 
   //SlidingUpPanel:
   final PanelController _panelController = PanelController();
@@ -48,23 +49,57 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     });
     _panelController.open();
     {
-      if (markers.value.isEmpty) return;
-
-      final points = [const LatLng(18.920164, -99.233274), position];
+      final points = [userPosition, targetPosition];
       animatedMapController.animatedFitCamera(
         cameraFit: CameraFit.coordinates(
-          coordinates: points,
-          padding: const EdgeInsets.all(180),
+          coordinates:
+              points.where((point) => point != null).cast<LatLng>().toList(),
+          padding: const EdgeInsets.all(80),
         ),
         rotation: 0,
-        customId: useTransformer ? useTransformerId : null,
+        customId: _useTransformer ? _useTransformerId : null,
       );
     }
   }
 
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Por favor, habilita los servicios de ubicación.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Los permisos de ubicación son denegados');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Los permisos de ubicación están permanentemente denegados, no podemos solicitar permisos.');
+    }
+
+    var userPositionDetermined = await Geolocator.getCurrentPosition();
+    setState(() {
+      userPosition = LatLng(
+          userPositionDetermined.latitude, userPositionDetermined.longitude);
+    });
+    return userPositionDetermined;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition();
+  }
+
   @override
   void dispose() {
-    markers.dispose();
     animatedMapController.dispose();
     super.dispose();
   }
@@ -80,9 +115,9 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
       body: Stack(
         children: <Widget>[
           HomeSectionMap(
+            userPosition: userPosition,
             targetPosition: targetPosition,
             animatedMapController: animatedMapController,
-            markers: markers,
           ),
           HomeSectionSearch(setTargetPosition: setTargetPosition),
 
@@ -148,7 +183,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
             right: 20,
             child: FloatingActionButton(
               onPressed: () => animatedMapController.animatedZoomOut(
-                customId: useTransformer ? useTransformerId : null,
+                customId: _useTransformer ? _useTransformerId : null,
               ),
               tooltip: 'Zoom out',
               child: const Icon(Icons.zoom_out),
--- lib/widgets/home/home-section-map/home_section_map.dart
+++ lib/widgets/home/home-section-map/home_section_map.dart
@@ -6,9 +6,6 @@ import 'package:flutter_map/flutter_map.dart';
 //Latlong2:
 import 'package:latlong2/latlong.dart';
 
-//Geolocator:
-import 'package:geolocator/geolocator.dart';
-
 //FlutterMapAnimations:
 import 'package:flutter_map_animations/flutter_map_animations.dart';
 
@@ -16,14 +13,14 @@ const mapboxAccessToken =
     "sk.eyJ1IjoicmotZGV2ZWxvcGVyIiwiYSI6ImNsc2dkazgzdTFsbjIybG8wMmFtcXVwODMifQ.gJl_3nLWEv_E9SeT6H_PkQ";
 
 class HomeSectionMap extends StatefulWidget {
+  final LatLng? userPosition;
   final LatLng? targetPosition;
   final AnimatedMapController animatedMapController;
-  final ValueNotifier<List<AnimatedMarker>> markers;
   const HomeSectionMap({
     Key? key,
+    required this.userPosition,
     required this.targetPosition,
     required this.animatedMapController,
-    required this.markers,
   }) : super(key: key);
 
   @override
@@ -31,95 +28,54 @@ class HomeSectionMap extends StatefulWidget {
 }
 
 class _HomeSectionMapState extends State<HomeSectionMap> {
-  LatLng? userPosition;
-
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Por favor, habilita los servicios de ubicación.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Los permisos de ubicación son denegados');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Los permisos de ubicación están permanentemente denegados, no podemos solicitar permisos.');
-    }
-
-    var userPositionDetermined = await Geolocator.getCurrentPosition();
-    setState(() {
-      userPosition = LatLng(
-          userPositionDetermined.latitude, userPositionDetermined.longitude);
-    });
-    return userPositionDetermined;
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _determinePosition();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return userPosition != null
-        ? ValueListenableBuilder<List<AnimatedMarker>>(
-            valueListenable: widget.markers,
-            builder: (context, markers, _) {
-              return FlutterMap(
-                mapController: widget.animatedMapController.mapController,
-                options: MapOptions(
-                  initialCenter: userPosition!,
-                  initialZoom: 17,
-                  maxZoom: 22,
-                  minZoom: 9.5,
-                  /* onTap: (_, point) => _addMarker(point), */
-                ),
-                children: [
-                  TileLayer(
-                    urlTemplate:
-                        "https://api.mapbox.com/styles/v1/rj-developer/clsgye3i303gv01o88gzf40sf/tiles/256/{z}/{x}/{y}@2x?access_token=$mapboxAccessToken",
-                    additionalOptions: const {
-                      'accessToken': mapboxAccessToken,
+    return widget.userPosition != null
+        ? FlutterMap(
+            mapController: widget.animatedMapController.mapController,
+            options: MapOptions(
+              initialCenter: widget.userPosition!,
+              initialZoom: 17,
+              maxZoom: 22,
+              minZoom: 9.5,
+              /* onTap: (_, point) => _addMarker(point), */
+            ),
+            children: [
+              TileLayer(
+                urlTemplate:
+                    "https://api.mapbox.com/styles/v1/rj-developer/clsgye3i303gv01o88gzf40sf/tiles/256/{z}/{x}/{y}@2x?access_token=$mapboxAccessToken",
+                additionalOptions: const {
+                  'accessToken': mapboxAccessToken,
+                },
+              ),
+              AnimatedMarkerLayer(
+                markers: [
+                  AnimatedMarker(
+                    point: widget.userPosition!,
+                    builder: (_, animation) {
+                      final size = 50.0 * animation.value;
+                      return Icon(
+                        Icons.person_pin,
+                        size: size,
+                        color: const Color.fromARGB(255, 25, 176, 218),
+                      );
                     },
                   ),
-                  AnimatedMarkerLayer(
-                    markers: [
-                      AnimatedMarker(
-                        point: userPosition!,
-                        builder: (_, animation) {
-                          final size = 50.0 * animation.value;
-                          return Icon(
-                            Icons.person_pin,
-                            size: size,
-                            color: const Color.fromARGB(255, 25, 176, 218),
-                          );
-                        },
-                      ),
-                      if (widget.targetPosition != null)
-                        AnimatedMarker(
-                          point: widget.targetPosition!,
-                          builder: (_, animation) {
-                            final size = 50.0 * animation.value;
-                            return Icon(
-                              Icons.location_pin,
-                              size: size,
-                              color: const Color.fromARGB(255, 170, 39, 39),
-                            );
-                          },
-                        ),
-                    ],
-                  ),
-                  /* AnimatedMarker(
+                  if (widget.targetPosition != null)
+                    AnimatedMarker(
+                      point: widget.targetPosition!,
+                      builder: (_, animation) {
+                        final size = 50.0 * animation.value;
+                        return Icon(
+                          Icons.location_pin,
+                          size: size,
+                          color: const Color.fromARGB(255, 170, 39, 39),
+                        );
+                      },
+                    ),
+                ],
+              ),
+              /* AnimatedMarker(
                         point: const LatLng(18.940714, -99.241622),
                         builder: (_, animation) {
                           final size = 50.0 * animation.value;
@@ -130,7 +86,7 @@ class _HomeSectionMapState extends State<HomeSectionMap> {
                           );
                         },
                       ), */
-                  /* PolylineLayer(
+              /* PolylineLayer(
                   polylines: [
                     Polyline(
                       strokeWidth: 4.0,
@@ -139,9 +95,7 @@ class _HomeSectionMapState extends State<HomeSectionMap> {
                     ),
                   ],
                 ), */
-                ],
-              );
-            },
+            ],
           )
         : const Center(child: CircularProgressIndicator());
   }
