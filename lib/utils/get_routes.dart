2256a7a39075b679555c29940bd3f65c0d4c0abd
RJ Luengas <robertojla8@gmail.com>
2024-03-04 23:26:33 -0600

    Se hizo avances en la lógica y se determina la mejor ruta a tomar con base a los trayectos definidos(Actualmente solo se coloca uno)

--- lib/utils/get_routes.dart
+++ lib/utils/get_routes.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+//Dart:
+import 'dart:math';
 import 'dart:convert';
 
 //Latlong2:
@@ -22,31 +25,88 @@ const dataRoutes = {
   ],
 };
 
-String getCoords(controlsMapProvider) {
-  var _route = [
-    controlsMapProvider.userPosition,
-    controlsMapProvider.targetPosition,
-  ];
-
-  return _route
+String getCoords(List<LatLng> route) {
+  return route
       .map((waypoint) => '${waypoint.longitude},${waypoint.latitude}')
       .join(';');
 }
 
-Future<List<LatLng>> getRoute(BuildContext context) async {
+class Ruta {
+  String nombre;
+  double latitudInicio;
+  double longitudInicio;
+  double latitudFin;
+  double longitudFin;
+
+  Ruta(this.nombre, this.latitudInicio, this.longitudInicio, this.latitudFin,
+      this.longitudFin);
+}
+
+Ruta encontrarRutaMasCercana(controlsMapProvider) {
+  List<LatLng> puntosDestino = [
+    controlsMapProvider.userPosition,
+    controlsMapProvider.targetPosition,
+  ];
+  double distanciaMinima = double.infinity;
+  Ruta? rutaMasCercana;
+
+  for (var entry in dataRoutes.entries) {
+    var puntosInicio = entry.value;
+    double distancia = sqrt(
+        pow(puntosDestino[0].latitude - puntosInicio[0].latitude, 2) +
+            pow(puntosDestino[0].longitude - puntosInicio[0].longitude, 2));
+
+    if (distancia < distanciaMinima) {
+      distanciaMinima = distancia;
+      rutaMasCercana = Ruta(
+          entry.key,
+          puntosInicio[0].latitude,
+          puntosInicio[0].longitude,
+          puntosInicio[2].latitude,
+          puntosInicio[2].longitude);
+    }
+  }
+
+  if (rutaMasCercana != null) {
+    print('Ruta más cercana: ${rutaMasCercana.nombre}');
+    return rutaMasCercana;
+  } else {
+    throw Exception('No se encontró ninguna ruta cercana.');
+  }
+}
+
+Future<void> getRoute(BuildContext context) async {
   final controlsMapProvider =
       Provider.of<ControlsMapProvider>(context, listen: false);
-  final response = await http.get(Uri.parse(
-      'https://api.mapbox.com/directions/v5/mapbox/driving/${getCoords(controlsMapProvider)}?geometries=geojson&access_token=$mapboxAccessToken'));
-  if (response.statusCode == 200) {
-    final data = json.decode(response.body);
-    final geometry = data['routes'][0]['geometry']['coordinates'];
-    final List<LatLng> points =
-        geometry.map<LatLng>((coord) => LatLng(coord[1], coord[0])).toList();
+  Ruta mejorRuta = encontrarRutaMasCercana(controlsMapProvider);
 
-    controlsMapProvider.route = points;
-    return points;
-  } else {
-    throw Exception('Failed to load _route');
+  String rutaCoords = getCoords([
+    LatLng(mejorRuta.latitudInicio, mejorRuta.longitudInicio),
+    LatLng(mejorRuta.latitudFin, mejorRuta.longitudFin),
+  ]);
+
+  try {
+    final response = await http.get(Uri.parse(
+        'https://api.mapbox.com/directions/v5/mapbox/driving/$rutaCoords?geometries=geojson&access_token=$mapboxAccessToken'));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final geometry = data['routes'][0]['geometry']['coordinates'];
+      final List<LatLng> points =
+          geometry.map<LatLng>((coord) => LatLng(coord[1], coord[0])).toList();
+
+/* final List<LatLng> rutaOptimizada = points
+          .where((point) =>
+              point.latitude == mejorRuta.latitudInicio &&
+              point.longitude == mejorRuta.longitudInicio)
+          .toList();
+
+      controlsMapProvider.route = rutaOptimizada; */
+      controlsMapProvider.route = points;
+    } else {
+      throw Exception('Failed to load route');
+    }
+  } catch (e) {
+    print('Error al obtener la ruta detallada: $e');
   }
 }
