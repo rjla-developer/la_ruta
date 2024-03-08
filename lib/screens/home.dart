2b704b81e21ebcd1beed17699676d27721d2a139
RJ Luengas <robertojla8@gmail.com>
2024-03-08 09:26:31 -0600

    Se subio un archivo zip con la lógica que se tendrá para el conjunto de archivos GTFS, los cuales son el estándar para el transporte público, para saber sus paradas, ubicaciones, tiempos, figura del recorrido, etc. Se hizo la lógica para obtener los datos de la figura que hace el trayecto de la ruta de ejemplo. Y se muestra en la app con un botón, esto nos ayudara a obtener una ruta con base a las coordenadas de cada ruta y determinar cual será el mejor trayecto y ruta para tomar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+//Dart:
+import 'dart:convert';
+import 'package:archive/archive.dart';
+import 'package:csv/csv.dart';
 
 //Widgets:
 import 'package:la_ruta/widgets/home/home-section-map/home_section_map.dart';
 import 'package:la_ruta/widgets/home/home_section_search.dart';
 import 'package:la_ruta/widgets/home/home_section_panel.dart';
 
-//Functions:
-/* import 'package:la_ruta/utils/get_routes.dart'; */
-
 //FlutterMapAnimations:
 import 'package:flutter_map_animations/flutter_map_animations.dart';
+import 'package:latlong2/latlong.dart';
 
 //SlidingUpPanel:
 import 'package:sliding_up_panel/sliding_up_panel.dart';
@@ -37,6 +41,43 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     super.dispose();
   }
 
+  void loadGtfsData(BuildContext context) async {
+    final controlsMapProvider =
+        Provider.of<ControlsMapProvider>(context, listen: false);
+
+    final byteData = await rootBundle.load('assets/gtfs/ruta3_ahuatlan.zip');
+    final bytes = byteData.buffer.asUint8List();
+    final archive = ZipDecoder().decodeBytes(bytes);
+
+    /* for (var file in archive) {
+      print(file.name);
+    } */
+
+    final shapesFile = archive.findFile('ruta3_ahuatlan/shapes.txt');
+
+    if (shapesFile != null) {
+      final shapesData = utf8.decode(shapesFile.content);
+      final lines = shapesData.split('\n');
+      List<List<String>> shapes = [];
+
+      for (var line in lines) {
+        var fields = line.split(',');
+        shapes.add(fields);
+      }
+
+      List<LatLng> routePoints = [];
+
+      for (int i = 1; i < shapes.length; i++) {
+        var shape = shapes[i];
+        double lat = double.parse(shape[1]);
+        double lon = double.parse(shape[2]);
+        routePoints.add(LatLng(lat, lon));
+      }
+
+      controlsMapProvider.route = routePoints;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final controlsMapProvider = context.watch<ControlsMapProvider>();
@@ -53,6 +94,15 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
           ),
           HomeSectionSearch(
             animatedMapController: animatedMapController,
+          ),
+          Positioned(
+            bottom: 100,
+            right: 20,
+            child: FloatingActionButton(
+              onPressed: () => loadGtfsData(context),
+              tooltip: 'Zoom out',
+              child: const Icon(Icons.remove_red_eye),
+            ),
           ),
           Positioned(
             bottom: 20,
